4707f0493f79f4f8f21721ed9cc7ee3206905013
stnava <stnava@gmail.com>
2025-06-09 18:36:07 -0400

    WIP: first time setup test

--- src/first_timer.py
+++ src/first_timer.py
@@ -1,4 +1,16 @@
+import os
+import ants
 import antspyt1w
 import antspymm
-antspyt1w.get_data(force_download=True)
-antspymm.get_data(force_download=True)
+
+template_path = os.path.expanduser("~/.antspymm/PPMI_template0.nii.gz")
+
+if os.path.exists(template_path):
+    template = ants.image_read(template_path)
+else:
+    # Run fallback code here
+    print("Template file not found. Running fallback procedure...")
+    antspyt1w.get_data(force_download=True)
+    antspymm.get_data(force_download=True)
+
+print("ready for test run")
